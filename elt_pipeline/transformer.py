36c7b356d97461576ec9d372427f3e5542265ea1
ghost-mann <iamaob8@protonmail.com>
2025-04-29 06:04:02 +0300

    feat: added methods to drop null values and duplicates in transformer.py

---
+++ elt_pipeline/transformer.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+class Transformer:
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+
+    def drop_null(self):
+        self.df = self.df.dropna()
+
+    def drop_duplicates(self):
+        self.df = self.df.drop_duplicates()
+
+    def get_data(self):
+        return self.df
+
