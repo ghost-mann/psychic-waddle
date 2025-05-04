3682102c87009f619290955ece9dd395eccade78
ghost-mann <iamaob8@protonmail.com>
2025-05-04 20:09:15 +0300

    feat: added comments in extractor.py

--- elt_pipeline/extractor.py
+++ elt_pipeline/extractor.py
@@ -3,17 +3,18 @@ import requests
 from sqlalchemy import create_engine
 
 class Extractor:
+    # from csv
     @staticmethod
     def from_csv(file_path):
         return pd.read_csv(file_path)
-
+    # from api
     @staticmethod
     def from_api(url):
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
         return pd.json_normalize(data)
-
+    # from db
     @staticmethod
     def from_db(connection_string,query):
         engine = create_engine(connection_string)
