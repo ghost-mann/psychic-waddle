d7376c9c7a9e0e302f8a1cbc9433921e5ab19c0b
ghost-mann <iamaob8@protonmail.com>
2025-05-04 19:59:08 +0300

    feat: added .env to git-ignore, added connection string in .env for db_connection.py

--- elt_pipeline/db_connection.py
+++ elt_pipeline/db_connection.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
 class DatabaseConnection:
-    def __init__(self, connection_string):
-        self.connection_string = connection_string
+    def __init__(self, env_var_name="DB_CONNECTION"):
+        self.connection_string = os.getenv(env_var_name)
+        if not self.connection_string:
+            raise ValueError(f"{env_var_name} environment variable is not set")
         self.engine = None
 
     def connect(self):
